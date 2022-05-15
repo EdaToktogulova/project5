e79dc889363ca42b95876de14fea99985c797c02
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-15 22:27:02 +0600

    New logo jpeg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Logo from '../assets/logo.jpeg';
+import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import '../styles/Navbar.css';
 
 
 function Navbar() {
@@ -7,8 +10,22 @@ function Navbar() {
       <div className='navbar'>
          <div className='leftSide'>
             <img src={Logo} />
+            <div className='hiddenLinks'>
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            </div>
          </div>
-         <div className='rightSide'></div>
+         <div className='rightSide'>
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <button>
+               <ReorderIcon />
+            </button>
+         </div>
       </div>
    )
 }
