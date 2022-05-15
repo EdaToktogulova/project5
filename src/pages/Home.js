4dae785edaa542f5df85b9cd0c667de874a208e9
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-16 00:43:48 +0600

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from '../assets/header.jpeg';
+import '../styles/Home.css';
 
 function Home() {
    return (
-      <div>
-         hello
+      <div className="home"  style={{ backgroundImage:  `url(${BannerImage   })`}}>
+         <div className="headerContainer">
+            <h1>Ice-Cream <br/> Shop</h1> <br /> <br /> <br />
+            <p>ICE-CREAM TO FIT ANY TASTE</p>
+            <Link to="/menu">
+               <button>ORDER NOW</button>
+            </Link>
+         </div>
       </div>
    )
 }
