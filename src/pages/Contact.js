191c0bb15df0085a9d4c07fd63ab8f15570d62ee
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-16 15:45:31 +0600

    Add new pages Contact

---
+++ src/pages/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CreamLeft from '../assets/creamLeft.jpeg'
+import '../styles/Contact.css';
+
+function Contact() {
+   return (
+      <div className='contact'>
+         <div className='leftSide' 
+         style={{backgroundImage: `url(${CreamLeft})`}}></div>
+         <div className='rightSide'>
+            <h1> Contact Us</h1>
+
+            <form id="contact-form" method="POST">
+               <label htmlFor="name">Full name</label>
+               <input id="name" placeholder="Enter full name..." type="text" />
+               <label htmlFor="email">Email</label>
+               <input id="email" placeholder="Enter email..." type="email" />
+               <label htmlFor="messsage">Messsage</label>
+               <textarea rows="6" placeholder="Enter message..." name="message" required> </textarea>
+               <button type="submit"> Send Messsage</button>
+
+            </form>
+         </div>
+      </div>
+   )
+}
+
+export default Contact;
