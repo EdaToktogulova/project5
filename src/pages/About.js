2cb952e7837e4f7349bc4ac04affb59cc6ecb5e6
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-16 15:45:49 +0600

    Add new pages About

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,12 +5,13 @@ import '../styles/About.css';
 function About() {
    return (
       <div className="about">
-         <div className="aboutTop" 
-         style={{ backgroundImage: `url(${IceCream})` }}>
+         <div className="aboutTop"
+            style={{ backgroundImage: `url(${IceCream})` }}>
          </div>
          <div className="aboutBottom">
             <h1>ABOUT US</h1>
-            <p>loresfjjjjjjjjjjjjnvjbshjfhjehjfihnvuesfigtophhdsf</p>
+            <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam id atque expedita aspernatur cupiditate vero, molestiae rem quaerat adipisci qui, quidem accusantium cum. Ullam officiis eaque cupiditate sunt rem! Voluptas pariatur perspiciatis doloremque libero praesentium quo, non reiciendis tempora exercitationem molestiae dolorum repellat voluptatibus ad impedit ex sed, sint nihil illum cumque aperiam harum modi, facilis vel vero! Perspiciatis dolores temporibus, ad architecto nulla totam quae! Veniam obcaecati quis nemo culpa repudiandae quisquam quo amet molestias fugiat neque atque repellat eos omnis, iste iusto sit, deleniti doloribus laudantium. Ullam at animi incidunt quaerat non quidem sapiente quos perspiciatis tenetur reprehenderit.</p>
          </div>
       </div>
    );
