333d537418212e22c9eb4d913090b0c385b66de2
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-16 10:07:20 +0600

    Add new component MenuList.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
 import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
 
 
@@ -12,6 +14,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/about" element={<About />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/helpers/MenuList.js
@@ -0,0 +1,35 @@
+import Menu1 from "../assets/menu1.jpeg";
+
+
+export const MenuList = [
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+   {
+      name: "lorem",
+      image: Menu1,
+      price: 12.99,
+   },
+];
