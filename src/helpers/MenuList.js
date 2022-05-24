c12cff4a940c6b257e95194fd2494a2cf8a83ed5
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-24 16:34:22 +0600

    Feat

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -9,38 +9,42 @@ import Menu7 from "../assets/menu7.jpeg";
 import Menu8 from "../assets/menu8.jpeg";
 import Menu9 from "../assets/menu9.jpeg";
 import Menu10 from "../assets/menu10.jpeg";
+import Menu11 from "../assets/menu11.jpeg";
+import Menu12 from "../assets/menu12.jpeg";
+import Menu13 from "../assets/menu13.jpeg";
+import Menu14 from "../assets/menu14.jpeg";
 
 
 export const MenuList = [
    {
       name: "Ice-cream cake",
       image: Menu,
-      price: 25,
+      price: 68,
    },
    {
       name: " Chocolate lux",
       image: Menu1,
-      price: 35,
+      price: 70,
    },
    {
       name: "Whipped cream",
       image: Menu2,
-      price: 30,
+      price: 50,
    },
    {
       name: "Ninety-nine",
       image: Menu3,
-      price: 50,
+      price: 83,
    },
    {
       name: "Frozen-yogurt",
       image: Menu4,
-      price: 36,
+      price: 79,
    },
    {
       name: "Cookies cream",
       image: Menu5,
-      price: 40,
+      price: 65,
    },
    {
       name: "Sunday Ice-Cream",
@@ -50,41 +54,41 @@ export const MenuList = [
    {
       name: "Banana Split",
       image: Menu7,
-      price: 40,
+      price: 59,
    },
    {
       name: "Classic Oreo",
       image: Menu8,
-      price: 40,
+      price: 61,
    },
    {
       name: "Avocado Ice-Cream",
       image: Menu9,
-      price: 40,
+      price: 76,
    },
    {
       name: "Chocolate Sandwiches",
       image: Menu10,
-      price: 40,
+      price: 92,
    },
    {
-      name: "Cookies cream",
-      image: Menu5,
-      price: 40,
+      name: "Rolled Ice-Cream",
+      image: Menu11,
+      price: 99,
    },
    {
-      name: "Cookies cream",
-      image: Menu5,
-      price: 40,
+      name: "Azuki red bean ice-cream",
+      image: Menu12,
+      price: 100,
    },
    {
-      name: "Cookies cream",
-      image: Menu5,
-      price: 40,
+      name: "Easy Chocolate Ice-Cream",
+      image: Menu13,
+      price: 74,
    },
    {
-      name: "Cookies cream",
-      image: Menu5,
-      price: 40,
+      name: "Midnight Chocolate",
+      image: Menu14,
+      price: 89,
    },
 ];
