d8fc341dc92a29c5b60231ac955dcee09f37872f
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-06 14:02:20 +0600

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,6 @@
 import React from "react";
 import '../styles/Home.css';
 
-import Food from '../components/food/Food.jsx';
-
-// Import images
-import Craft1 from "../assets/craft1.jpeg"
-import Craft2 from "../assets/craft2.jpeg"
-import Violet1 from "../assets/violet1.jpeg"
-import Violet2 from "../assets/violet2.jpeg"
 
 const Header = () => {
    return (
