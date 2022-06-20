ed357302b0ea08ebdd750c2b590aab023e34f898
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-20 15:00:39 +0600

    Added formatting

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 import IceCream from "../assets/iceCream.jpeg";
-import '../styles/About.css';
+import "../styles/About.css";
 
 function About() {
    return (
       <div className="about">
-         <div className="aboutTop"
+         <div
+            className="aboutTop"
             style={{ backgroundImage: `url(${IceCream})` }}>
          </div>
          <div className="aboutBottom">
             <h1>ABOUT US</h1>
             <p>
-
-               ‘Lux Ice-Cream’ is another American dessert parlor, concentrated on serving surface-driven little group frozen yogurts with a recharged consideration regarding flavor and sense of taste. Proprietor and originator build up every formula explicitly for every individual flavor.
-
-               The uprightness of his fixings is never traded off with added substances or handled balancing out.</p>
+               ‘Lux Ice-Cream’ is another American dessert parlor, concentrated on
+               serving surface-driven little group frozen yogurts with a recharged
+               consideration regarding flavor and sense of taste. Proprietor and
+               originator build up every formula explicitly for every individual
+               flavor. The uprightness of his fixings is never traded off with added
+               substances or handled balancing out.
+            </p>
          </div>
       </div>
    );
