31ec19b1c5f9174b8f9c5838f31ebd690c4f3d03
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-23 14:23:24 +0600

    changed h1 to About component

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,12 +5,9 @@ import "../styles/About.css";
 function About() {
    return (
       <div className="about">
-         <div
-            className="aboutTop"
-            style={{ backgroundImage: `url(${IceCream})` }}>
-         </div>
+         <div className="aboutTop" style={{ backgroundImage: `url(${IceCream})` }}></div>
          <div className="aboutBottom">
-            <h1>ABOUT US</h1>
+            <h1>About Us</h1>
             <p>
                ‘Lux Ice-Cream’ is another American dessert parlor, concentrated on
                serving surface-driven little group frozen yogurts with a recharged
