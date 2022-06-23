0afd2f23042452fdc215eaec83becd51e96f71c7
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-23 14:25:04 +0600

    Added teg textarea to  the  component  Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,18 +5,18 @@ import '../styles/Contact.css';
 function Contact() {
    return (
       <div className='contact'>
-         <div className='leftSide' 
-         style={{backgroundImage: `url(${CreamLeft})`}}></div>
+         <div className='leftSide'
+            style={{ backgroundImage: `url(${CreamLeft})` }}>
+         </div>
          <div className='rightSide'>
-            <h1> Contact Us</h1>
-
+            <h1>Contact Us</h1>
             <form id="contact-form" method="POST">
                <label htmlFor="name">Full name</label>
                <input id="name" placeholder="Enter full name..." type="text" />
                <label htmlFor="email">Email</label>
                <input id="email" placeholder="Enter email..." type="email" />
                <label htmlFor="messsage">Messsage</label>
-               <textarea  rows="6" placeholder="Enter message..." name="message" required />
+               <textarea rows="6" placeholder="Enter message..." name="message" required />
                <button type="submit"> Send Messsage</button>
             </form>
          </div>
