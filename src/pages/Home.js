c6ef2540e346064bc19db9a29cfeef5e3b3365bd
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-23 12:44:41 +0600

    Add new component Food

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,32 @@
 import React from "react";
+import Food from "../components/Food/Food";
 import '../styles/Home.css';
+
+import Craft1 from "../assets/craft1.jpeg";
+import Craft2 from "../assets/craft2.jpeg";
+import Violet1 from "../assets/violet1.jpeg";
+import Violet2 from "../assets/violet2.jpeg";
 
 
 const Header = () => {
    return (
-      <div className="header">
-         <div className="container">
-            <div className="content">
-               <div className="text">
-                  <p>welcome to our</p>
+      <div>
+         <div className="header">
+            <div className="container">
+               <div className="content">
+                  <div className="text">
+                     <p>welcome to our</p>
+                  </div>
+                  <h1>Ice-Cream</h1>
+                  <h1>Shop</h1>
+                  <button className="btn">Delivery</button>
+                  <button className="btn">Take-out</button>
                </div>
-               <h1>Ice-Cream</h1>
-               <h1>Shop</h1>
-               <button className="btn">Delivery</button>
-               <button className="btn">Take-out</button>
             </div>
          </div>
+         <Food bgImg1={Craft1} bgImg2={Craft2} />
+         <Food bgImg={Violet1} />
+         <Food bgImg={Violet2} />
       </div>
    );
 }
