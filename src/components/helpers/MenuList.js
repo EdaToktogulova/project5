a9becc1ed482a57eacf632a66b42197101f4ede4
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-29 10:07:55 +0600

    Added pictures and imports to pictures in MenuList

---
+++ src/components/helpers/MenuList.js
@@ -0,0 +1,94 @@
+import menu from "../../assets/menu.jpeg";
+import menu1 from "../../assets/menu1.jpeg";
+import menu2 from "../../assets/menu2.jpeg";
+import menu3 from "../../assets/menu3.jpeg";
+import menu4 from "../../assets/menu4.jpeg";
+import menu5 from "../../assets/menu5.jpeg";
+import menu6 from "../../assets/menu6.jpeg";
+import menu7 from "../../assets/menu7.jpeg";
+import menu8 from "../../assets/menu8.jpeg";
+import menu9 from "../../assets/menu9.jpeg";
+import menu10 from "../../assets/menu10.jpeg";
+import menu11 from "../../assets/menu11.jpeg";
+import menu12 from "../../assets/menu12.jpeg";
+import menu13 from "../../assets/menu13.jpeg";
+import menu14 from "../../assets/menu14.jpeg";
+
+
+export const MenuList = [
+   {
+      name: "Ice-cream cake",
+      image: menu,
+      price: 68,
+   },
+   {
+      name: " Chocolate lux",
+      image: menu1,
+      price: 70,
+   },
+   {
+      name: "Whipped cream",
+      image: menu2,
+      price: 69,
+   },
+   {
+      name: "Ninety-nine",
+      image: menu3,
+      price: 83,
+   },
+   {
+      name: "Frozen-yogurt",
+      image: menu4,
+      price: 79,
+   },
+   {
+      name: "Cookies cream",
+      image: menu5,
+      price: 65,
+   },
+   {
+      name: "Sunday Ice-Cream",
+      image: menu6,
+      price: 40,
+   },
+   {
+      name: "Banana Split",
+      image: menu7,
+      price: 59,
+   },
+   {
+      name: "Classic Oreo",
+      image: menu8,
+      price: 61,
+   },
+   {
+      name: "Avocado Ice-Cream",
+      image: menu9,
+      price: 76,
+   },
+   {
+      name: "Chocolate Sandwiches",
+      image: menu10,
+      price: 92,
+   },
+   {
+      name: "Rolled Ice-Cream",
+      image: menu11,
+      price: 99,
+   },
+   {
+      name: "Azuki red bean ice-cream",
+      image: menu12,
+      price: 100,
+   },
+   {
+      name: "Easy Chocolate Ice-Cream",
+      image: menu13,
+      price: 74,
+   },
+   {
+      name: "Midnight Chocolate",
+      image: menu14,
+      price: 89,
+   },
+];
