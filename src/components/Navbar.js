d245e46c53dae209bac2ae6d4e3d574bcfbbae9f
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-29 10:10:08 +0600

    Add react-router-dom in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaIceCream, FaBars, FaTimes } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import "../styles/Navbar.css";
 
 
